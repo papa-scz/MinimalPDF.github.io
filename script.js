3b377b432f237f3f8040a3742875ab7c9c55e469
papa-scz <159469165+papa-scz@users.noreply.github.com>
2024-02-23 16:02:00 +0100

    Update script.js
    retiré acces via url

--- script.js
+++ script.js
@@ -1,88 +1,58 @@
 import * as pdfjsLib from './lib/pdfjs/pdf.mjs';
 
-// Configuration du worker
 pdfjsLib.GlobalWorkerOptions.workerSrc = './lib/pdfjs/pdf.worker.mjs';
 
 document.addEventListener('DOMContentLoaded', function() {
-    const fileInput = document.querySelector('input[type="file"]');
-    const urlInput = document.querySelector('input[type="url"]');
+    const fileInput = document.getElementById('file-input');
     const pdfViewer = document.getElementById('pdf-viewer');
     const versionNumber = '1.0.1';
     const versionElement = document.getElementById('version');
 
-    // Sélectionner l'élément et mettre à jour son contenu
     versionElement.innerText += versionNumber;
 
-    // Gérer le chargement de PDF par URL
-    urlInput.addEventListener('change', function() {
-        const url = urlInput.value;
-        if (url) {
-            // Ici, vous pouvez utiliser PDF.js pour charger et afficher le PDF
-            console.log('Chargement du PDF depuis l\'URL:', url);
-            loadPdf(url);
-        }
-    });
-
-    // Gérer le chargement de PDF par sélection de fichier
     fileInput.addEventListener('change', function(event) {
         const file = event.target.files[0];
         if (file) {
-            // Ici, vous pouvez utiliser PDF.js pour charger et afficher le PDF
-            console.log('Chargement du PDF depuis un fichier:', file.name);
             loadPdfFromFile(file);
         }
     });
 
-    // Gérer le glisser-déposer
-    const dropZone = document.body; // Utilisez `document.body` ou un élément spécifique comme zone de dépôt
+    const dropZone = document.body;
     dropZone.addEventListener('dragover', function(event) {
-        event.preventDefault(); // Empêcher le comportement par défaut
+        event.preventDefault();
     });
     dropZone.addEventListener('drop', function(event) {
         event.preventDefault();
         const file = event.dataTransfer.files[0];
         if (file) {
-            // Ici, vous pouvez utiliser PDF.js pour charger et afficher le PDF
-            console.log('Chargement du PDF depuis le glisser-déposer:', file.name);
             loadPdfFromFile(file);
         }
     });
 
-    // Exemple de fonction pour charger un PDF (à implémenter avec PDF.js ou autre)
-    function loadPdf(url) {
-    pdfjsLib.getDocument(url).promise.then(function(pdfDoc) {
-        console.log("Le PDF est chargé");
-
-        pdfDoc.getPage(1).then(function(page) {
-            var canvas = document.createElement('canvas');
-            var ctx = canvas.getContext('2d');
-            // Assurez-vous que `getViewport` est appelé avec un objet de configuration.
-            var viewport = page.getViewport({ scale: 1.5 });
-
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            var renderContext = {
-                canvasContext: ctx,
-                viewport: viewport
-            };
-            page.render(renderContext);
-
-            pdfViewer.innerHTML = '';
-            pdfViewer.appendChild(canvas);
-        });
-    }).catch(function(error) {
-        console.log("Erreur lors du chargement du PDF :", error);
-    });
-}
-
-
-
     function loadPdfFromFile(file) {
-    const url = URL.createObjectURL(file);
-    loadPdf(url);
-    // N'oubliez pas de libérer l'URL de l'objet quand elle n'est plus nécessaire
-    URL.revokeObjectURL(url);
-}
+        const url = URL.createObjectURL(file);
+        pdfjsLib.getDocument(url).promise.then(function(pdfDoc) {
+            console.log("Le PDF est chargé");
+            pdfDoc.getPage(1).then(function(page) {
+                var canvas = document.createElement('canvas');
+                var ctx = canvas.getContext('2d');
+                var viewport = page.getViewport({ scale: 1.5 });
 
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                var renderContext = {
+                    canvasContext: ctx,
+                    viewport: viewport
+                };
+                page.render(renderContext);
+
+                pdfViewer.innerHTML = '';
+                pdfViewer.appendChild(canvas);
+            });
+        }).catch(function(error) {
+            console.log("Erreur lors du chargement du PDF :", error);
+        });
+        URL.revokeObjectURL(url);
+    }
 });
